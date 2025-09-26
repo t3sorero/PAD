eaf3a28a1c8a50c5c75a48a2505455f0ccde7bb9
t3sorero <jmartint@ucm.es>
2025-09-26 15:48:22 +0200

    principio actividad 1 a falta de la suma en la otra pestaña

--- app/src/main/java/es/ucm/fdi/pad/android01/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/pad/android01/MainActivity.java
@@ -1,6 +1,9 @@
 package es.ucm.fdi.pad.android01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button sumar = findViewById(R.id.button);
+        sumar.setOnClickListener(View ->{
+            EditText sumando1 = findViewById(R.id.editTextNumberDecimal3);
+            EditText sumando2 = findViewById(R.id.editTextNumberDecimal4);
+            float res;
+            if (!sumando1.getText().toString().isEmpty() && !sumando2.getText().toString().isEmpty())
+                res = Float.parseFloat(sumando1.getText().toString()) + Float.parseFloat(sumando2.getText().toString());
         });
     }
 }
